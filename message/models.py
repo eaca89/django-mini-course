08f132e19b9b60678777723581bb23a5ddbbb140
Ehsan <eaca89@gmail.com>
2023-06-16 17:17:25 -0400

    message board is ready.

--- message/models.py
+++ message/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Message(models.Model):
+    text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.text
--- message/views.py
+++ message/views.py
@@ -1,7 +1,9 @@
-from django.views.generic import TemplateView
-
+from django.views.generic import ListView
+from .models import Message
 # def messageView(request):
 #     return render(request, 'home.html')
 
-class MessageView(TemplateView):
+
+class MessageView(ListView):
+    model = Message
     template_name = 'home.html'
